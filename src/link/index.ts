95508883c59185ba998d12b8f382735413b2a4ef
zkochan <zoltan.kochan@gmail.com>
2017-03-29 22:03:51 +0300

    feat: proper resolving of peer dependencies
    BREAKING CHANGE: structure of node_modules changed
    
    Close #678, PR #694

--- src/link/index.ts
+++ src/link/index.ts
@@ -6,17 +6,17 @@ import exists = require('path-exists')
 import logger from 'pnpm-logger'
 import R = require('ramda')
 import globalBinPath = require('global-bin-path')
+import pLimit = require('p-limit')
 import {InstalledPackage} from '../install/installMultiple'
 import {InstalledPackages} from '../api/install'
 import linkBins from './linkBins'
 import {Package} from '../types'
-import resolvePeers from './resolvePeers'
+import resolvePeers, {ResolvedDependencyTree, ResolvedDependencyTreeMap} from './resolvePeers'
 
 export type LinkedPackage = {
   id: string,
   pkg: Package,
-  hardlinkedLocation: string,
-  modules: string,
+  localLocation: string,
   path: string,
   fetchingFiles: Promise<boolean>,
   dependencies: string[],
@@ -34,35 +34,34 @@ export default async function (
     global: boolean,
     baseNodeModules: string,
   }
-): Promise<LinkedPackagesMap> {
+): Promise<ResolvedDependencyTreeMap> {
   const pkgsToLink = await resolvePeers(R.values(installedPkgs)
-    .filter(installedPkg => installedPkg.isInstallable)
     .reduce((pkgsToLink, installedPkg) => {
-      const modules = path.join(opts.baseNodeModules, `.${installedPkg.id}`, 'node_modules')
       pkgsToLink[installedPkg.id] = {
         id: installedPkg.id,
         pkg: installedPkg.pkg,
         fetchingFiles: installedPkg.fetchingFiles,
-        modules,
-        hardlinkedLocation: path.join(modules, installedPkg.pkg.name),
+        localLocation: path.join(opts.baseNodeModules, `.${installedPkg.id}`),
         path: installedPkg.path,
         dependencies: installedPkg.dependencies,
       }
       return pkgsToLink
-    }, {}))
+    }, {}), topPkgs.filter(pkg => pkg.isInstallable).map(pkg => pkg.id))
 
-  for (let id of R.keys(pkgsToLink)) {
-    await linkPkg(pkgsToLink[id], opts)
-  }
+  const flatResolvedDeps =  R.values(pkgsToLink).sort((a, b) => a.depth - b.depth)
 
-  for (let id of R.keys(pkgsToLink)) {
-    await linkModules(pkgsToLink[id], pkgsToLink)
-  }
+  const deps = <ResolvedDependencyTree[]>R.uniqBy(R.prop('hardlinkedLocation'), flatResolvedDeps)
 
-  for (let pkg of topPkgs) {
-    if (!pkg.isInstallable) continue
-    const dest = path.join(opts.baseNodeModules, pkg.pkg.name)
-    await symlinkDir(pkgsToLink[pkg.id].hardlinkedLocation, dest)
+  await linkAllPkgs(deps, opts)
+
+  const depsByModules = <ResolvedDependencyTree[]>R.uniqBy(R.prop('modules'), flatResolvedDeps)
+
+  await linkAllIndependentModules(depsByModules, pkgsToLink)
+
+  await linkAllModules(deps, pkgsToLink)
+
+  for (let pkg of flatResolvedDeps.filter(pkg => pkg.depth === 0)) {
+    await symlinkDependencyTo(pkg, opts.baseNodeModules)
   }
   const binPath = opts.global ? globalBinPath() : path.join(opts.baseNodeModules, '.bin')
   await linkBins(opts.baseNodeModules, binPath)
@@ -70,25 +69,59 @@ export default async function (
   return pkgsToLink
 }
 
+const limitLinking = pLimit(16)
+
+async function linkAllPkgs (
+  alldeps: ResolvedDependencyTree[],
+  opts: {
+    force: boolean,
+    global: boolean,
+    baseNodeModules: string,
+  }
+) {
+  return Promise.all(
+    alldeps.map(pkg => limitLinking(() => linkPkg(pkg, opts)))
+  )
+}
+
+async function linkAllModules (
+  pkgs: ResolvedDependencyTree[],
+  pkgMap: ResolvedDependencyTreeMap
+) {
+  return Promise.all(
+    pkgs.map(pkg => limitLinking(() => linkModules(pkg, pkgMap)))
+  )
+}
+
+async function linkAllIndependentModules (
+  pkgs: ResolvedDependencyTree[],
+  pkgMap: ResolvedDependencyTreeMap
+) {
+  return Promise.all(
+    pkgs.map(pkg => limitLinking(() => linkIndependentModules(pkg, pkgMap)))
+  )
+}
+
 async function linkPkg (
-  dependency: LinkedPackage,
+  dependency: ResolvedDependencyTree,
   opts: {
     force: boolean,
     baseNodeModules: string,
   }
 ) {
   const newlyFetched = await dependency.fetchingFiles
+
   const pkgJsonPath = path.join(dependency.hardlinkedLocation, 'package.json')
-  if (newlyFetched || opts.force || !await exists(pkgJsonPath) || !await pkgLinkedToStore()) {
+  if (newlyFetched || opts.force || !await exists(pkgJsonPath) || !await pkgLinkedToStore(pkgJsonPath, dependency)) {
     await linkDir(dependency.path, dependency.hardlinkedLocation)
   }
+}
 
-  async function pkgLinkedToStore () {
-    const pkgJsonPathInStore = path.join(dependency.path, 'package.json')
-    if (await isSameFile(pkgJsonPath, pkgJsonPathInStore)) return true
-    logger.info(`Relinking ${dependency.hardlinkedLocation} from the store`)
-    return false
-  }
+async function pkgLinkedToStore (pkgJsonPath: string, dependency: ResolvedDependencyTree) {
+  const pkgJsonPathInStore = path.join(dependency.path, 'package.json')
+  if (await isSameFile(pkgJsonPath, pkgJsonPathInStore)) return true
+  logger.info(`Relinking ${dependency.hardlinkedLocation} from the store`)
+  return false
 }
 
 async function isSameFile (file1: string, file2: string) {
@@ -96,23 +129,36 @@ async function isSameFile (file1: string, file2: string) {
   return stats[0].ino === stats[1].ino
 }
 
+async function linkIndependentModules (pkg: ResolvedDependencyTree, pkgMap: ResolvedDependencyTreeMap) {
+  await Promise.all(
+    R.props<ResolvedDependencyTree>(pkg.children, pkgMap)
+      .map(child => symlinkDependencyTo(child, pkg.modules))
+  )
+}
+
 async function linkModules (
-  dependency: LinkedPackage,
-  pkgsToLink: LinkedPackagesMap
+  dependency: ResolvedDependencyTree,
+  pkgMap: ResolvedDependencyTreeMap
 ) {
-  for (let depId of dependency.dependencies) {
-    const subdep = pkgsToLink[depId]
-    if (!subdep) continue
-    const dest = path.join(dependency.modules, subdep.pkg.name)
-    await symlinkDir(subdep.hardlinkedLocation, dest)
-  }
+  await Promise.all(
+    R.props<ResolvedDependencyTree>(dependency.resolvedPeers, pkgMap)
+      .map(peer => symlinkDependencyTo(peer, <string>dependency.peerModules))
+  )
 
   const binPath = path.join(dependency.hardlinkedLocation, 'node_modules', '.bin')
-  await linkBins(dependency.modules, binPath, dependency.pkg.name)
+  await linkBins(dependency.modules, binPath, dependency.name)
+  if (dependency.peerModules) {
+    await linkBins(dependency.peerModules, binPath, dependency.name)
+  }
 
   // link also the bundled dependencies` bins
-  if (dependency.pkg.bundledDependencies || dependency.pkg.bundleDependencies) {
+  if (dependency.hasBundledDependencies) {
     const bundledModules = path.join(dependency.hardlinkedLocation, 'node_modules')
     await linkBins(bundledModules, binPath)
   }
+}
+
+function symlinkDependencyTo (dependency: ResolvedDependencyTree, dest: string) {
+  dest = path.join(dest, dependency.name)
+  return symlinkDir(dependency.hardlinkedLocation, dest)
 }
--- src/link/resolvePeers.ts
+++ src/link/resolvePeers.ts
@@ -1,39 +1,147 @@
-import path = require('path')
-import semver = require('semver')
-import {LinkedPackagesMap} from '.'
-import logger from 'pnpm-logger'
+import {LinkedPackagesMap, LinkedPackage} from '.'
+import {Dependencies, Package} from '../types'
 import R = require('ramda')
+import semver = require('semver')
+import logger from 'pnpm-logger'
+import path = require('path')
 
-type PackageVersions = {
-  [version: string]: string
+export type DependencyTree = {
+  id: string,
+  name: string,
+  version: string,
+  peerDependencies: Dependencies,
+  hasBundledDependencies: boolean,
+  fetchingFiles: Promise<boolean>,
+  localLocation: string,
+  path: string,
+  keypathId: string,
+  children: string[],
+  depth: number,
 }
 
-type InstalledPackageVersions = {
-  [pkgName: string]: PackageVersions
+export type ResolvedDependencyTree = {
+  name: string,
+  hasBundledDependencies: boolean,
+  path: string,
+  modules: string,
+  peerModules?: string,
+  fetchingFiles: Promise<boolean>,
+  hardlinkedLocation: string,
+  children: string[],
+  resolvedPeers: string[],
+  depth: number,
+  id: string,
 }
 
-export default async function linkPeers (pkgs: LinkedPackagesMap): Promise<LinkedPackagesMap> {
-  const groupedPkgs: InstalledPackageVersions = {}
+export type ResolvedDependencyTreeMap = {
+  [keypathId: string]: ResolvedDependencyTree
+}
 
-  R.values(pkgs).forEach(pkgData => {
-    if (!pkgData.pkg.version) return
+export default function (pkgsMap: LinkedPackagesMap, topPkgIds: string[]): ResolvedDependencyTreeMap {
+  const treeMap = {}
+  const rootNodeIds = createTree(pkgsMap, topPkgIds, [], treeMap)
 
-    const pkgName = pkgData.pkg.name
-    groupedPkgs[pkgName] = groupedPkgs[pkgName] || {}
-    groupedPkgs[pkgName][pkgData.pkg.version] = pkgData.id
-  })
+  const pkgsByName = toPkgByName(R.props<DependencyTree>(rootNodeIds, treeMap))
+  const resolvedTreeMap = R.reduce(R.merge, {}, rootNodeIds.map(rootNodeId => resolvePeersOfNode(rootNodeId, pkgsByName, treeMap)))
+  return resolvedTreeMap
+}
 
-  await Promise.all(R.values(pkgs).map(async pkgData => {
-    const peerDependencies = pkgData.pkg.peerDependencies || {}
-    await Promise.all(Object.keys(peerDependencies).map(async peerName => {
-      const version = semver.maxSatisfying(Object.keys(groupedPkgs[peerName] || {}), peerDependencies[peerName], true)
-      if (!version) {
-        logger.warn(`${pkgData.id} requires a peer of ${peerName}@${peerDependencies[peerName]} but none was installed.`)
-        return
+function createTree (pkgsMap: LinkedPackagesMap, pkgIds: string[], keypath: string[], treeMap: {[keypathId: string]: DependencyTree}): string[] {
+  return R.props(pkgIds, pkgsMap)
+    .map((pkg: LinkedPackage) => {
+      const nonCircularDeps = getNonCircularDependencies(pkg.id, pkg.dependencies, keypath)
+      const newKeypath = R.append(pkg.id, keypath)
+      const keypathId = newKeypath.join('/')
+      treeMap[keypathId] = {
+        id: pkg.id,
+        name: pkg.pkg.name,
+        version: pkg.pkg.version,
+        peerDependencies: pkg.pkg.peerDependencies || {},
+        hasBundledDependencies: !!(pkg.pkg.bundledDependencies || pkg.pkg.bundleDependencies),
+        fetchingFiles: pkg.fetchingFiles,
+        localLocation: pkg.localLocation,
+        path: pkg.path,
+        keypathId,
+        children: createTree(pkgsMap, nonCircularDeps, newKeypath, treeMap),
+        depth: keypath.length,
       }
-      pkgData.dependencies.push(groupedPkgs[peerName][version])
-    }))
-  }))
+      return keypathId
+    })
+}
 
-  return pkgs
+function getNonCircularDependencies (
+  parentId: string,
+  dependencyIds: string[],
+  keypath: string[]
+) {
+  const relations = R.aperture(2, keypath)
+  const isCircular = R.partialRight(R.contains, [relations])
+  return dependencyIds.filter(depId => !isCircular([parentId, depId]))
+}
+
+function resolvePeersOfNode (
+  keypathId: string,
+  parentPkgs: {[name: string]: DependencyTree},
+  treeMap: {[keypathId: string]: DependencyTree}
+): ResolvedDependencyTreeMap {
+  const pkgNode = treeMap[keypathId]
+  const newParentPkgs = Object.assign({}, parentPkgs,
+    {[pkgNode.name]: pkgNode},
+    toPkgByName(R.props<DependencyTree>(pkgNode.children, treeMap))
+  )
+
+  const resolvedPeers = resolvePeers(pkgNode.peerDependencies, pkgNode.id, newParentPkgs)
+
+  const modules = path.join(pkgNode.localLocation, 'node_modules')
+  const peerModules = !R.isEmpty(pkgNode.peerDependencies)
+    ? path.join(pkgNode.localLocation, createPeersFolderName(R.props<DependencyTree>(resolvedPeers, treeMap)), 'node_modules')
+    : undefined
+
+  const hardlinkedLocation = path.join(peerModules || modules, pkgNode.name)
+
+  return R.reduce(R.merge, R.objOf(keypathId, {
+    name: pkgNode.name,
+    hasBundledDependencies: pkgNode.hasBundledDependencies,
+    fetchingFiles: pkgNode.fetchingFiles,
+    path: pkgNode.path,
+    peerModules,
+    modules,
+    hardlinkedLocation,
+    resolvedPeers,
+    children: pkgNode.children,
+    depth: pkgNode.depth,
+    id: pkgNode.id,
+  }), pkgNode.children.map(child => resolvePeersOfNode(child, newParentPkgs, treeMap)))
+}
+
+function resolvePeers (
+  peerDependencies: Dependencies,
+  pkgId: string,
+  parentPkgs: {[name: string]: DependencyTree},
+): string[] {
+  return R.toPairs(peerDependencies)
+    .map(R.apply((peerName: string, peerVersionRange: string) => {
+      const resolved = parentPkgs[peerName]
+
+      if (!resolved) {
+        logger.warn(`${pkgId} requires a peer of ${peerName}@${peerVersionRange} but none was installed.`)
+        return null
+      }
+
+      if (!semver.satisfies(resolved.version, peerVersionRange)) {
+        logger.warn(`${pkgId} requires a peer of ${peerName}@${peerVersionRange} but version ${resolved.version} was installed.`)
+      }
+
+      return resolved && resolved.keypathId
+    }))
+    .filter(Boolean) as string[]
+}
+
+function toPkgByName(pkgs: DependencyTree[]): {[pkgName: string]: DependencyTree} {
+  const toNameAndPkg = R.map((pkg: DependencyTree): R.KeyValuePair<string, DependencyTree> => [pkg.name, pkg])
+  return R.fromPairs(toNameAndPkg(pkgs))
+}
+
+function createPeersFolderName(peers: DependencyTree[]) {
+  return peers.map(peer => `${peer.name.replace('/', '!')}@${peer.version}`).sort().join('+')
 }
